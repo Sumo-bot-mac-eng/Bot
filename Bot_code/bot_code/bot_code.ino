769238bc277683ba0727f58d0544e91f1195fd69
2547technowizz <neil_edward_lobo@hotmail.com>
2020-03-10 22:22:25 -0400

    added loop code
    code is still in development as we've had some issues with the hardware

--- Bot_code/bot_code/bot_code.ino
+++ Bot_code/bot_code/bot_code.ino
@@ -8,6 +8,8 @@ const int trigPin = 11;
 const int echoPin = 12;
 const int IN_A0 = A0;
 const int IN_D0 = 8;
+const bool LEFT = 0;
+const bool RIGHT = 1;
 
 
 void setup() {
@@ -22,11 +24,46 @@ void setup() {
   pinMode (IN_D0, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//  setMotor(RIGHT, HIGH, LOW, 200);
+//  Serial.print(myUltra());
+//  Serial.println(" cm");
+//  Serial.println(line_sensor());
+//  setMotor(RIGHT, HIGH, LOW, 200);
+//  delay(500);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB, 255);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA, 85);
+//  Serial.println(digitalRead(12));
+//  analogWrite(enB, 200);
+//  Serial.print(digitalRead(2));
+//  Serial.print(" ");
+//  Serial.println(digitalRead(3));
+//  delay(500); 
+  
+  
+}
 
+void setMotor(bool side, bool dir1, bool dir2, int speed) //side: left=0, right =1; 
+{
+  if(side == 0)
+  {
+    digitalWrite(in1, dir1);
+    digitalWrite(in2, dir2);
+    analogWrite(enA, speed);
+  }
+  else
+  {
+    digitalWrite(in2, dir1);
+    digitalWrite(in2, dir2);
+    analogWrite(enB, speed);
+  }
 }
 
 //call myMotor() in loop
@@ -53,14 +90,14 @@ void myMotor()
 
 }
 
-void line_sensor()
+float line_sensor()
 {
   int value_A0;
   bool value_D0;
   
   value_A0 = analogRead(IN_A0); // reads the analog input from the IR distance sensor
   value_D0 = digitalRead(IN_D0);// reads the digital input from the IR distance sensor
-  Serial.println(value_A0);
+  return value_A0;
 
   
   delay(1000);
@@ -70,7 +107,7 @@ long duration;
 int distance;
 
 //call myUltra() in loop
-void myUltra()
+float myUltra()
 {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -84,7 +121,6 @@ void myUltra()
   // Calculating the distance
   distance= duration*0.034/2;
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance in CM: ");
-  Serial.println(distance);
+  return distance; //in cm
 
 }
